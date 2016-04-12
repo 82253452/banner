ccf00af27d81adeba577c78d5c7378f35bbacd16
82253452 <82253452@qq.com>
2016-04-13 02:15:30 +0800

    delete angular2 demo

--- src/main/resources/templates/Angular2Sample-master/README.md
+++
@@ -1,6 +0,0 @@
-
-### Usage
-- Make sure you have [node.js](https://nodejs.org/) installed
-- run `npm install` to install dependencies
-- run `npm start` to fire up dev server
-- open browser to `http://localhost:8080`
--- src/main/resources/templates/angular2/app/app.js
+++
@@ -1,9 +0,0 @@
-import {bootstrap, ChangeDetection, jitChangeDetection} from 'angular2/angular2';
-import {bind} from 'angular2/di';
-import {Calendar} from 'components/calendar/calendar';
-import {Wechat} from 'components/wechat/wechat';
-import {CalendarCell} from 'components/calendar-cell/calendar-cell';
-
-export function main() {
-  return bootstrap(Wechat);
-}
