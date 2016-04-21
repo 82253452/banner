4cb627040facb2cf390e8d283ec92a5d43b40f3f
82253452 <82253452@qq.com>
2016-04-22 02:03:11 +0800

    New changelist

--- src/main/resources/templates/admin/components/wechat/wechat.ts
+++ src/main/resources/templates/admin/components/wechat/wechat.ts
@@ -1,7 +1,8 @@
-import {Component,NgFor,HTTP_PROVIDERS} from 'angular2/core';
+import {Component, NgFor, click, HTTP_PROVIDERS} from 'angular2/core';
+import { FORM_DIRECTIVES } from 'angular2/common';
 import {Http} from 'angular2/http';
 import {Router,ROUTER_DIRECTIVES} from 'angular2/router';
-import {WechatService} from 'service/wechat/WechatService';
+import {LyWeInfoService} from 'service/base/LyWeInfoService';
 import {Common} from 'common/Common';
 import 'rxjs/add/operator/map';
 import 'rxjs/Rx';
@@ -10,19 +11,28 @@ import 'rxjs/Rx';
 @Component({
   selector: '#page-wrapper',
   templateUrl: System.baseURL+'admin/components/wechat/wechat.html',
-  providers: [WechatService,Common],
-  directives: [],
+  providers: [LyWeInfoService,Common],
+  directives: [FORM_DIRECTIVES],
   pipes: []
 })
 
 export class Wechat {
   public wechatList;
-  constructor(private common : Common,private wechatService : WechatService ) {
-   this.wechatService.getWechatList().then(data=>
+  constructor(
+      private common : Common ,
+      private wechatService : LyWeInfoService
+  ) {
+   this.wechatService.List().then(data=>
        {
          this.wechatList=data;
        }
-    );
-  }
+   );
+      }
+    add(wechat){
+        console.info(wechat);
+        this.wechatService.add(wechat).then(data=>{
+            console.info(data);
+        });
+    }
 }
 
