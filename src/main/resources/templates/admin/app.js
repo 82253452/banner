9b8c2f26ea3aa2ffd960f05187f1d436a884867e
82253452 <82253452@qq.com>
2016-04-26 02:03:24 +0800

    upate page layout

--- src/main/resources/templates/admin/app.js
+++ src/main/resources/templates/admin/app.js
@@ -1,12 +1,11 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
-import {app} from './app';
-import {Wechat} from 'components/wechat/wechat';
+import {Home} from 'components/home/home';
 
 
 /*export function main() {
   return bootstrap(Wechat);
 }*/
-bootstrap(Wechat, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
+bootstrap(Home, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
     .catch(err => console.error(err));
